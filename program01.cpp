ab19ca1d95fc488a36421bf2d72e8a32335dd6f1
Sidra123-kanwal <kanwalsidra2006@gmail.com>
2025-12-09 23:49:39 +0500

    Add files via upload

---
+++ program01.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+ {
+    int a = 10;
+    float b = 5;
+    int c = 6;
+
+    cout << "Value of a: " << a << endl;
+    cout << "Value of b: " << b << endl;
+    cout << "Value of c: " << c << endl;
+
+    return 0;
+}
